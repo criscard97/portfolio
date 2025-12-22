e1fb89dfcdbee3d14b1258c6c12809ba0eb78cd3
criscard97 <cristian.hernandez.ca@usb.edu.mx>
2025-12-22 17:02:36 -0600

    Mejoramiento en los toast con IziToast

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,11 +1,19 @@
 const email = "crismcard17@gmail.com";
 
-  document.getElementById("copyEmail").addEventListener("click", () => {
-    navigator.clipboard.writeText(email)
-      .then(() => {
-        alert("Correo copiado al portapapeles 📋");
-      })
-      .catch(err => {
-        console.error("Error al copiar", err);
+document.getElementById("copyEmail").addEventListener("click", () => {
+  navigator.clipboard.writeText(email)
+    .then(() => {
+      iziToast.info({
+        title: '',
+        message: 'Correo copiado al portapapeles!',
+        position: 'topCenter',
+        icon:'',
+        image: '../img/clipboardicon.svg',
+        imageWidth: 35
       });
-  });
+
+    })
+    .catch(err => {
+      console.error("Error al copiar", err);
+    });
+});
